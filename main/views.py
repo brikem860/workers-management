7f0108b3fc12e1ed6297e4e287f0bf02eaecd4d7
brikem860 <bryanmeli2020@gmail.com>
2024-01-31 13:18:50 +0300

    first commit of my project

---
+++ main/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render, HttpResponse
+
+
+# Create your views here.
+def home(request):
+    return render(request, 'main/home.html')
+
+
+def workers(request):
+    return render(request, 'main/workers.html')
