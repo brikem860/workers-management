3d5bf05ea3756be182f9ce71d80c5e1079683d30
brikem860 <bryanmeli2020@gmail.com>
2024-02-04 12:00:24 +0300

    third commit of my project

--- main/views.py
+++ main/views.py
@@ -1,10 +1,26 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
+from .forms import RegistrationForm
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.decorators import login_required
 
 
+@login_required(login_url="/login")
 # Create your views here.
 def home(request):
     return render(request, 'main/home.html')
 
 
+@login_required(login_url="/login")
 def workers(request):
     return render(request, 'main/workers.html')
+
+
+def sign_up(request):
+    if request.method == "POST":
+        form = RegistrationForm(request.POST)
+        user = form.save()
+        login(request, user)
+        return redirect('/home')
+    else:
+        form = RegistrationForm()
+    return render(request, 'registration/sign_up.html', {'form': form})
--- workersmanagement/settings.py
+++ workersmanagement/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
@@ -26,7 +25,6 @@ SECRET_KEY = 'django-insecure-n*1xswa8_eiqcq*(p1!2y_qg87!+ah3s0x!l-^+y#3i$n$$5-d
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -75,7 +73,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'workersmanagement.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
@@ -89,7 +86,6 @@ DATABASES = {
         'PORT': '10680'
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
@@ -109,7 +105,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
@@ -121,7 +116,6 @@ USE_I18N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
@@ -131,3 +125,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+LOGIN_REDIRECT_URL = '/home'
+LOGOUT_REDIRECT_URL = '/login'
